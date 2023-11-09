18762089d0de9a41e678c2be0c3d8a51add5bcc0
JuGon314 <julianamathematic@yahoo.com>
2023-11-09 19:37:32 -0300

    Novas pastas adicionadas para organizar melhor os desafios

---
+++ Segundo desafio/segundo-desafio.js
@@ -0,0 +1,24 @@
+// let vitorias = getElementById("vitorias").value;
+// let derrotas = getElementById("derrotas").value;
+
+function dadosJogador(vitorias, derrotas) {
+    saldoVitorias = vitorias - derrotas;
+    
+    if(saldoVitorias < 10) {
+        console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de Ferro`)
+    } else if (saldoVitorias >= 11 && saldoVitorias < 20) {
+        console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de Bronze`)
+    } else if (saldoVitorias >= 21 && saldoVitorias < 50) {
+        console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de Prata`)
+    } else if (saldoVitorias >= 51 && saldoVitorias < 80) {
+        console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de Ouro`)
+    } else if (saldoVitorias >= 81 && saldoVitorias < 90) {
+        console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de Diamante`)
+    } else if (saldoVitorias >= 91 && saldoVitorias < 100) {
+        console.log(`O Herói tem saldo de ${saldoVitorias} está no nível de Lendário`)
+    } else {
+        console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de Imortal`)
+    }
+}
+
+dadosJogador(102, 2)
